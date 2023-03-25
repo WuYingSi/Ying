b627cd3a25ea843fe9f91c0e9ebe086ce752fa06
WuYingSi <1722779068@qq.com>
2023-03-25 15:18:49 +0800

    提交到仓库

---
+++ Java/Ying-Java/src/base/ying/day/Nine/对象_商品/test.java
@@ -0,0 +1,10 @@
+package base.ying.day.Nine.对象_商品;
+
+import java.util.Scanner;
+
+public class test {
+  public static void main(String[] args) {
+    Scanner sb=new Scanner(System.in);
+    int nextInt = sb.nextInt();
+  }
+}
