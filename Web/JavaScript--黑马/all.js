ae0c4dd61c91b6279405d2f45a0bb3ec063a92f3
WuYingSi <1722779068@qq.com>
2023-05-12 00:09:48 +0800

    5.12

---
+++ Web/JavaScript--黑马/all.js
@@ -0,0 +1,11 @@
+    // setInterval          定时器/间歇函数
+    // addEventListener     事件监听
+    // 事件类型
+    // mouseenter           鼠标经过
+    // click                鼠标点击
+    // mouseleave           鼠标离开
+    // focus                获得焦点
+    // blur                 失去焦点
+    // Keydown              键盘按下触发
+    // Keyup                键盘抬起触发
+    // input                用户输入事件
