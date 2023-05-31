7ae5f3ab78708cb2ff39da0d94eb8500228ad509
WuYingSi <1722779068@qq.com>
2023-06-01 00:35:13 +0800

    6.1

---
+++ Java/Ying-Java/src/Study/Day/day_11/案例/P2_遍历集合.java
@@ -0,0 +1,34 @@
+package Study.Day.day_11.案例;
+
+import java.util.ArrayList;
+
+public class P2_遍历集合 {
+    //创建一个存储字符串的集合，存储3个字符串元素，使用程序实现在控制台遍历该集合
+    public static void main(String[] args) {
+        //1.创建集合对象
+        ArrayList<String> list = new ArrayList<>();
+
+        //2.添加元素
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("ddd");
+
+        //3.遍历
+        //快捷键: list.fori 正向遍历
+        //list.forr 倒着遍历
+        System.out.print("[");
+        for (int i = 0; i < list.size(); i++) {
+            //i 依次表示集合里面的每一个索引
+
+            if(i == list.size() - 1){
+                //最大索引
+                System.out.print(list.get(i));
+            }else{
+                //非最大索引
+                System.out.print(list.get(i) + ", ");
+            }
+        }
+        System.out.print("]");
+    }
+}
