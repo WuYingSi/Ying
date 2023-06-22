d6cffe9846ca3eebc9a0f367a5d34882fdacaf27
WuYingSi <1722779068@qq.com>
2023-06-22 14:49:49 +0800

    6.22

---
+++ Java/SutdyJava/src/day_13/Case/P5/JavaBean/Animal.java
@@ -0,0 +1,11 @@
+package day_13.Case.P5.JavaBean;
+
+public class Animal {
+    public void eat() {
+        System.out.println("吃饭");
+    }
+
+    public void drink() {
+        System.out.println("喝水");
+    }
+}
