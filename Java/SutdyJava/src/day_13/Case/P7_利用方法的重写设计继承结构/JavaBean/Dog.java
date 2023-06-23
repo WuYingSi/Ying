565d1c1da30ec4fb429bc909a46f3ef3f2888412
WuYingSi <1722779068@qq.com>
2023-06-23 15:05:23 +0800

    6.23

---
+++ Java/SutdyJava/src/day_13/Case/P7_利用方法的重写设计继承结构/JavaBean/Dog.java
@@ -0,0 +1,16 @@
+package day_13.Case.P7_利用方法的重写设计继承结构.JavaBean;
+
+public class Dog {
+
+
+    public void eat() {
+        System.out.println("吃饭（吃狗粮）");
+    }
+
+    public void drink(){
+        System.out.println("喝水");
+    }
+    public void lookHome(){
+        System.out.println("看家");
+    }
+}
