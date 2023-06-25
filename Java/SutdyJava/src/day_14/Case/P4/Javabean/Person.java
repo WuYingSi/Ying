d275e7eb579df1917450642326607bf47e0ca7c9
WuYingSi <1722779068@qq.com>
2023-06-25 13:11:27 +0800

    6.25

--- Java/SutdyJava/src/day_14/Case/P4/Javabean/Person.java
+++ Java/SutdyJava/src/day_14/Case/P4/Javabean/Person.java
@@ -3,22 +3,36 @@ package day_14.Case.P4.Javabean;
 public class Person {
     private String name;
     private int age;
-    // 喂养宠物狗
-    public void keepPet(Dog dog, String something) {
-        System.out.println("年龄为"+age+"岁的"+name+"养了一只"+dog.getColor()+"颜色的"+dog.getAge()+"岁的狗");
-        dog.eat(something);
-    }
-    // 喂养宠物猫
-    public void keepPet(Cat cat, String something) {
-        System.out.println("年龄为"+age+"岁的"+name+"养了一只"+cat.getColor()+"颜色的"+cat.getAge()+"岁的猫");
-        cat.eat(something);
-    }
+
     public Person() {
     }
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+/*    // 喂养宠物狗
+    public void keepPet(Dog dog, String something) {
+        System.out.println("年龄为" + age + "岁的" + name + "养了一只" + dog.getColor() + "颜色的" + dog.getAge() + "岁的狗");
+        dog.eat(something);
+    }
+    // 喂养宠物猫
+    public void keepPet(Cat cat, String something) {
+        System.out.println("年龄为" + age + "岁的" + name + "养了一只" + cat.getColor() + "颜色的" + cat.getAge() + "岁的猫");
+        cat.eat(something);
+    }*/
+    // 喂养动物
+    public void keepPet(Animal a, String something){
+        // 对传递过来的动物对象进行判断 使用关键词instanceof
+        if (a instanceof Dog d){
+            System.out.println("年龄为" + age + "岁的" + name + "养了一只" + d.getColor() + "颜色的" + d.getAge() + "岁的狗");
+            d.eat(something);
+        }else if (a instanceof Cat c){
+            System.out.println("年龄为" + age + "岁的" + name + "养了一只" + c.getColor() + "颜色的" + c.getAge() + "岁的猫");
+            c.eat(something);
+        }
+
     }
 
     public String getName() {
