ebfddb12897ae49334286857af06b66dbc554199
WuYingSi <1722779068@qq.com>
2023-06-25 13:11:10 +0800

    6.25

--- Java/SutdyJava/src/day_14/Case/P4/Test.java
+++ Java/SutdyJava/src/day_14/Case/P4/Test.java
@@ -1,7 +1,21 @@
 package day_14.Case.P4;
 
+import day_14.Case.P4.Javabean.Cat;
+import day_14.Case.P4.Javabean.Dog;
+import day_14.Case.P4.Javabean.Person;
+
 public class Test {
     public static void main(String[] args) {
+        // 创建饲养员
+        Person r = new Person("小英", 21);
+        // 创建猫
+        Cat c = new Cat(2, "白");
+        // 创建狗
+        Dog d = new Dog(1, "黑");
+        c.catchMouse();
+        d.lookHome();
+        r.keepPet(d,"牛肉");
+        r.keepPet(c,"鱼肉");
 
     }
 }
