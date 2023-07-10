8c9440623da742a95366df72546de4aca1366193
WuYingSi <1722779068@qq.com>
2023-07-10 14:11:35 +0800

    7.10

---
+++ Java/SutdyJava/src/day_15/Concept/P3/类与接口基本实现/SportMan.java
@@ -0,0 +1,10 @@
+package day_15.Concept.P3.类与接口基本实现;
+
+public interface SportMan {
+    /**
+     接口：接口体现的是规范。
+     * */
+    void run(); // 抽象方法，跑步。
+    void law(); // 抽象方法，遵守法律。
+    String compittion(String project);  // 抽象方法，比赛。
+}
