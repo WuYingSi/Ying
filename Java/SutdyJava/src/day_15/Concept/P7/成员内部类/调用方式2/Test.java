617d1207d479079bc460292e280e80c7b3c2858c
WuYingSi <1722779068@qq.com>
2023-07-16 14:54:08 +0800

    7.16

---
+++ Java/SutdyJava/src/day_15/Concept/P7/成员内部类/调用方式2/Test.java
@@ -0,0 +1,10 @@
+package day_15.Concept.P7.成员内部类.调用方式2;
+
+public class Test {
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        System.out.println(o.getInstance());
+
+
+    }
+}
