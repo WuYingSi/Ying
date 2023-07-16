01e4de2309158ca5e3a8dd534508bc554c31b8a3
WuYingSi <1722779068@qq.com>
2023-07-16 14:53:56 +0800

    7.16

---
+++ Java/SutdyJava/src/day_15/Concept/P7/成员内部类/调用方式1/Outer.java
@@ -0,0 +1,13 @@
+package day_15.Concept.P7.成员内部类.调用方式1;
+
+class Outer {
+    // 成员内部类，属于外部类对象的。
+    // 拓展：成员内部类不能定义静态成员。
+    public class Inner{
+        // 这里面的东西与类是完全一样的。
+        public void method(){
+            System.out.println("内部类中的方法被调用了");
+        }
+    }
+}
+
