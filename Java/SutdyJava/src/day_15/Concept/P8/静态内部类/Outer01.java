f8d0daf62275b576aad656236641abf940b0574c
WuYingSi <1722779068@qq.com>
2023-07-16 14:54:19 +0800

    7.16

---
+++ Java/SutdyJava/src/day_15/Concept/P8/静态内部类/Outer01.java
@@ -0,0 +1,19 @@
+package day_15.Concept.P8.静态内部类;
+
+// 外部类：Outer01
+class Outer01{
+    private static  String sc_name = "黑马程序";
+    // 内部类: Inner01
+    public static class Inner01{
+        // 这里面的东西与类是完全一样的。
+        private String name;
+        public Inner01(String name) {
+            this.name = name;
+        }
+        public void showName(){
+            System.out.println(this.name);
+            // 拓展:静态内部类可以直接访问外部类的静态成员。
+            System.out.println(sc_name);
+        }
+    }
+}
